fe45dc08db905449a48ee291c8c4e66cc5292f64
Lukathis <xuchi16@126.com>
2024-08-22 16:30:27 +0800

    feat: init commit

---
+++ De3ign/De3ign/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  De3ign
+//
+//  Created by xuchi on 2024/8/22.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ De3ign/De3ignTests/De3ignTests.swift
@@ -0,0 +1,17 @@
+//
+//  De3ignTests.swift
+//  De3ignTests
+//
+//  Created by xuchi on 2024/8/22.
+//
+
+import Testing
+@testable import De3ign
+
+struct De3ignTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
