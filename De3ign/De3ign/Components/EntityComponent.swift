511ac1e0b0cd0bafda1a76d81016306d57bbce59
Lemocuber <tmiaotheyeeter@gmail.com>
2024-12-18 10:29:07 +0800

    feat: physics based drag (?)

--- De3ign/De3ign/Components/EntityComponent.swift
+++ De3ign/De3ign/Components/EntityComponent.swift
@@ -20,14 +20,54 @@ struct EditorMetadataComponent: Component {
     let source: EntitySource
 }
 
+private struct StartPositionComponent: Component {
+    var value: SIMD3<Float>? = nil
+}
+
 struct DragToMoveComponent: Component {
     let target: Entity
+    
+    init(target: Entity) {
+        self.target = target
+        target.components.set(StartPositionComponent())
+    }
+    
     func handleChange(_ event: EntityTargetValue<DragGesture.Value>) {
+        // move by force if has physics
         if var physicsBody = target.components[PhysicsBodyComponent.self] {
             physicsBody.isAffectedByGravity = false
-            target.components.set(physicsBody)
+            
+            if (target.components[StartPositionComponent.self]?.value == nil) {
+                target.components.set(StartPositionComponent(value: target.position(relativeTo: nil)))
+            }
+            let startPosition = target.components[StartPositionComponent.self]!.value!
+            let targetPosition = event.convert(event.location3D, from: .local, to: .scene)
+            let eventPosition = startPosition + targetPosition
+            let entityPosition = target.position(relativeTo: nil)
+            
+            print("\(entityPosition)-\(eventPosition)")
+            
+            let direction = eventPosition - entityPosition
+            var strength = length(direction)
+            
+            if strength < 1.0 {
+                strength *= strength
+            }
+            
+            print("\(direction):\(strength)")
+            
+            let forceFactor: Float = 0.01
+            let force = forceFactor * simd_normalize(direction)
+            
+            print(force)
+            
+            (target as! HasPhysicsBody).addForce(force, relativeTo: nil)
         }
-        target.position = event.convert(event.location3D, from: .local, to: target.parent!)
+        // move by setting position otherwise
+        else {
+            target.position = event.convert(event.location3D, from: .local, to: target.parent!)
+        }
+        // trigger on distance event when applicable
         if let interaction = target.components[InteractOnDistanceComponent.self] {
             if target.distance(to: interaction.other) < interaction.threshold {
                 interaction.callback()
@@ -39,6 +79,7 @@ struct DragToMoveComponent: Component {
     }
 
     func handleEnd(_ event: EntityTargetValue<DragGesture.Value>) {
+        target.components.set(StartPositionComponent())
         if var physicsBody = target.components[PhysicsBodyComponent.self] {
             physicsBody.isAffectedByGravity = true
             target.components.set(physicsBody)
